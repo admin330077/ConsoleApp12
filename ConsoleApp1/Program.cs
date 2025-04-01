038596689703811be4a9dc47d3f7f8b81c9bab58
admin330077 <keyyep900@gmail.com>
2025-04-01 16:47:32 +0300

    основа приложения

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Ку");
+﻿int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
